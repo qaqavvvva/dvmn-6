c2c288fd441be81d0baa0e274b7e6922b3a09977
Lana <129321333+qaqavvvva@users.noreply.github.com>
2023-04-05 06:11:18 +0300

    First commit

---
+++ main.py
@@ -0,0 +1,81 @@
+import file_operations
+import random
+from faker import Faker
+
+
+fake = Faker('ru_RU')
+
+dict = {
+    'а': 'а͠', 'б': 'б̋', 'в': 'в͒͠',
+    'г': 'г͒͠', 'д': 'д̋', 'е': 'е͠',
+    'ё': 'ё͒͠', 'ж': 'ж͒', 'з': 'з̋̋͠',
+    'и': 'и', 'й': 'й͒͠', 'к': 'к̋̋',
+    'л': 'л̋͠', 'м': 'м͒͠', 'н': 'н͒',
+    'о': 'о̋', 'п': 'п̋͠', 'р': 'р̋͠',
+    'с': 'с͒', 'т': 'т͒', 'у': 'у͒͠',
+    'ф': 'ф̋̋͠', 'х': 'х͒͠', 'ц': 'ц̋',
+    'ч': 'ч̋͠', 'ш': 'ш͒͠', 'щ': 'щ̋',
+    'ъ': 'ъ̋͠', 'ы': 'ы̋͠', 'ь': 'ь̋',
+    'э': 'э͒͠͠', 'ю': 'ю̋͠', 'я': 'я̋',
+    'А': 'А͠', 'Б': 'Б̋', 'В': 'В͒͠',
+    'Г': 'Г͒͠', 'Д': 'Д̋', 'Е': 'Е',
+    'Ё': 'Ё͒͠', 'Ж': 'Ж͒', 'З': 'З̋̋͠',
+    'И': 'И', 'Й': 'Й͒͠', 'К': 'К̋̋',
+    'Л': 'Л̋͠', 'М': 'М͒͠', 'Н': 'Н͒',
+    'О': 'О̋', 'П': 'П̋͠', 'Р': 'Р̋͠',
+    'С': 'С͒', 'Т': 'Т͒', 'У': 'У͒͠',
+    'Ф': 'Ф̋̋͠', 'Х': 'Х͒͠', 'Ц': 'Ц̋',
+    'Ч': 'Ч̋͠', 'Ш': 'Ш͒͠', 'Щ': 'Щ̋',
+    'Ъ': 'Ъ̋͠', 'Ы': 'Ы̋͠', 'Ь': 'Ь̋',
+    'Э': 'Э͒͠͠', 'Ю': 'Ю̋͠', 'Я': 'Я̋',
+    ' ': ' '
+}
+
+skills = [
+    'Стремительный прыжок',
+    'Электрический выстрел',
+    'Ледяной удар',
+    'Стремительный удар',
+    'Кислотный взгляд',
+    'Тайный побег',
+    'Ледяной выстрел',
+    'Огненный заряд'
+]
+
+
+def create_template():
+    skills_choice = random.sample(skills, 3)
+    runic_skills = []
+
+    for skill in skills_choice:
+        for letter, replacement in dict.items():
+            skill = skill.replace(letter, replacement)
+        runic_skills.append(skill)
+
+    context = {
+        'first_name': fake.first_name(),
+        'last_name': fake.last_name(),
+        'town': fake.job(),
+        'job': fake.city(),
+        'strength': str(random.randint(3, 18)),
+        'agility': str(random.randint(3, 18)),
+        'endurance': str(random.randint(3, 18)),
+        'intelligence': str(random.randint(3, 18)),
+        'luck': str(random.randint(3, 18)),
+        'skill_1': runic_skills[0],
+        'skill_2': runic_skills[1],
+        'skill_3': runic_skills[2]
+    }
+    return context
+
+
+def main():
+    for i in range(10):
+        context = create_template()
+        from_file = 'src/template.svg'
+        to_file = f'src/output/result_{i + 1}.svg'
+        file_operations.render_template(from_file, to_file, context)
+
+
+if __name__ == '__main__':
+    main()
